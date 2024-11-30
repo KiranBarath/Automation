1fbb36ce7295d0c075c5b3b272fdcc94e3562849
kiran <kiranraj98in@gmail.com>
2024-11-30 10:59:38 +0530

    2nd commit

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -20,7 +20,7 @@ public class PositiveTests {
 // 		Create driver
 		WebDriver driver = new ChromeDriver();
 		sleep(1);
-		// Just checking
+		// Just checking 2nd time
 
 // 		open test page
 		String url = "https://the-internet.herokuapp.com/login";
